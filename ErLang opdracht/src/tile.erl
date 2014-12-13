21b248adf5510126353d60c4fb44a9f63ef90e06
NeleR <smilieke611@hotmail.com>
2014-12-13 14:58:25 +0100

    up, zonder testen

--- ErLang opdracht/src/tile.erl
+++ ErLang opdracht/src/tile.erl
@@ -44,14 +44,33 @@ tilelife(Id,CurrentValue,Merged)->
 	tilelife( ).
 
 up(Id,Value) ->
+    if
+        % you are either an empty tile or a tile at the top
+		Value == 0 || Id - 4 < 0 ->					
+            Id ! {setvalue,Value,false};			% keep your current value
+            Id + 4 ! up								% tell the tile below you to calculate its up movement
+        % you are not empty and not the top
+		true ->
+            Id - 4 ! {yourValue, self()};			% ask the tile above you about its value
+    end,
     receive ->
         {tilevalue, NId, NValue, NMerged}; ->
             if
-                NValue == Value && NMerged == false ->
-                    NId - 4 ! {setvalue, NValue + Value, true};  % merge with the next tile
-                NValue == 0 && NId - 4 > 0 ->
+				% the tile above you has the same value and is not merged yet: you can merge
+				% or the tile above you is an empty tile and is the top: you can merge
+                (NValue == Value && !NMerged)
+				|| (NValue == 0 && NId - 4 < 0) ->						
+                    NId ! {setvalue, NValue + Value, true};		% add your value to the tile above you
+					Id ! {setvalue,0,false};						% make your own tile empty
+					Id + 4 ! up										% tell the tile below you to calculate its up movement
+				% the tile above you has another value or has been merged already: you cannot merge
+				NValue != Value || NMerged ->						
+					Id ! {setvalue,Value,false};					% keep your current value
+					Id + 4 ! up										% tell the tile below you to calculate its up movement
+                % the tile above you is an empty tile and not the top: you have to ask further
+				NValue == 0 ->										
                     NId - 4 ! {yourValue, self()};  % the responsive tile is empty, ask the next tile
                 true ->
-                    NId + 4 ! {setvalue, Value, false};  % you cannot merge
+                    % do nothing
             end,
     end,
